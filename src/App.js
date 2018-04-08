524d09a93eec19f1617a0bd5edfdd5d485ccc840
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-08 10:08:39 +0700

    config: add storybook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import media from 'styled-media-query'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import routes from './routes'
+import logo from './logo.svg'
+import './App.css'
+import { Button } from 'reactstrap'
+import Footer from './common/Footer'
+
+const Site = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  text-align: center;
+  ${media.lessThan('small')`
+    background: #222;
+  `};
+`
+
+const SiteContent = styled.div`
+  flex: 1;
+`
 
 class App extends Component {
-
-  state = {
-    persons: []
-  }
-
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const persons = res.data;
-        console.log(persons)
-        this.setState({ persons });
-      })
-  }
-
   render() {
+    const routeComponents = routes.map(
+      ({ path, component, exact, render }, key) => (
+        <Route
+          exact={exact}
+          path={path}
+          component={component}
+          key={key}
+          render={render}
+        />
+      )
+    )
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Site>
+        <SiteContent>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Button color="danger">Danger!</Button>
+            <Switch>
+              {routeComponents}
+              <Redirect to="/" />
+            </Switch>
+          </div>
+        </SiteContent>
+        <Footer />
+      </Site>
+    )
   }
 }
 
-export default App;
+export default App
