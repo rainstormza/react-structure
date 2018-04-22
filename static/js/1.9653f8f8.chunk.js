2f63933a6b4c3a5a524428ea065b3fda9aad5c7d
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-22 09:44:42 +0700

    Updates

---
+++ static/js/1.9653f8f8.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{1272:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=n(0),u=n.n(l),a=function(){return u.a.createElement("div",null,u.a.createElement("h2",null,"Test"))};t.default=a}});
+//# sourceMappingURL=1.9653f8f8.chunk.js.map
