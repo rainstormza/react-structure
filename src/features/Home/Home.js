6fa942dfd37c847788a81a9003cb020c0793cf87
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-22 09:21:28 +0700

    [✨] create and connect redux for Home

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Input } from 'antd'
+import { connect } from 'react-redux'
+import { setField } from './redux'
 
-const Home = () => {
+const Home = props => {
+  const { home, setField } = props
   return (
     <div>
       <h2>Home</h2>
+      {home.text}
+      <Input
+        placeholder="Basic usage"
+        onChange={e => setField(e.target.value)}
+      />
     </div>
   )
 }
 
-export default Home
+export default connect(state => state, { setField })(Home)
---
+++ src/features/Home/redux.js
@@ -0,0 +1,25 @@
+const SET_FIELD = 'SET_FIELD'
+
+const initialState = {
+  text: ''
+}
+
+const reducer = (state = initialState, action) => {
+  // console.log(state, action)
+  switch (action.type) {
+    case SET_FIELD:
+      return {
+        ...state,
+        text: action.value
+      }
+    default:
+      return state
+  }
+}
+export default reducer
+
+// action creator
+export const setField = value => ({
+  type: SET_FIELD,
+  value
+})
