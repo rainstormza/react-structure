7997b267e1b7468b09b43369e85867e260c01e52
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-22 09:29:26 +0700

    [🔧] add more actions and props to redux login

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -4,11 +4,26 @@ import logo from '../../logo.svg'
 import { Button } from 'antd'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { increment, decrement } from './redux'
+import {
+  increment,
+  decrement,
+  incrementAsync,
+  fetchData,
+  incrementAsyncEpic,
+  fetchDataEpic
+} from './redux'
 
 const Login = props => {
-  // console.log(props.counter)
-  const { counter, decrement, increment } = props
+  // console.log(props)
+  const {
+    login,
+    decrement,
+    increment,
+    incrementAsync,
+    fetchData,
+    incrementAsyncEpic,
+    fetchDataEpic
+  } = props
   return (
     <div>
       <div className="App">
@@ -24,16 +39,44 @@ const Login = props => {
             Login
           </Button>
         </Link>
-        <p>Counter: {counter}</p>
+        <p>Counter: {login.counter}</p>
         <Button onClick={decrement}>Decrement</Button>
         <Button onClick={increment}>Increment</Button>
+        <Button onClick={incrementAsync}>Increment Async</Button>
+        <Button onClick={incrementAsyncEpic}>Increment Async Epic</Button>
+        <br />
+        <Button
+          type="primary"
+          icon="reload"
+          loading={login.isLoading}
+          onClick={fetchData}
+        >
+          Click me!
+        </Button>
+        <p>{JSON.stringify(login.data)}</p>
+        <Button
+          type="primary"
+          icon="reload"
+          loading={login.isLoading}
+          onClick={fetchDataEpic}
+        >
+          Click me Epic!
+        </Button>
+        <p>{JSON.stringify(login.data)}</p>
       </div>
       {/* <h2>Login</h2> */}
     </div>
   )
 }
 
-export default connect(state => state, { increment, decrement })(Login)
+export default connect(state => state, {
+  increment,
+  decrement,
+  incrementAsync,
+  fetchData,
+  incrementAsyncEpic,
+  fetchDataEpic
+})(Login)
 
 // conntect(mapStateToProps, mapDispatchToProps)
 
--- src/features/Login/redux.js
+++ src/features/Login/redux.js
@@ -1,12 +1,21 @@
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
+const INCREMENT_ASYNC = 'INCREMENT_ASYNC'
+const INCREMENT_ASYNC_EPIC = 'INCREMENT_ASYNC_EPIC'
+
+const FETCH_REQUESTED = 'FETCH_REQUESTED'
+const FETCH_REQUESTED_EPIC = 'FETCH_REQUESTED_EPIC'
+const FETCH_SUCCEEDED = 'FETCH_SUCCEEDED'
+const FETCH_FAILED = 'FETCH_FAILED'
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  isLoading: false,
+  data: {}
 }
 
 const reducer = (state = initialState, action) => {
-  console.log(state, action)
+  // console.log(state, action)
   switch (action.type) {
     case INCREMENT:
       return {
@@ -17,6 +26,35 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         counter: state.counter - 1
+      }
+    case INCREMENT_ASYNC:
+      return {
+        ...state
+      }
+    case INCREMENT_ASYNC_EPIC:
+      return {
+        ...state
+      }
+    case FETCH_REQUESTED:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case FETCH_REQUESTED_EPIC:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case FETCH_SUCCEEDED:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.data
+      }
+    case FETCH_FAILED:
+      return {
+        ...state,
+        isLoading: false
       }
     default:
       return state
@@ -31,4 +69,20 @@ export const increment = () => ({
 
 export const decrement = () => ({
   type: DECREMENT
+})
+
+export const incrementAsync = () => ({
+  type: INCREMENT_ASYNC
+})
+
+export const incrementAsyncEpic = () => ({
+  type: INCREMENT_ASYNC_EPIC
+})
+
+export const fetchData = () => ({
+  type: FETCH_REQUESTED
+})
+
+export const fetchDataEpic = () => ({
+  type: FETCH_REQUESTED_EPIC
 })
