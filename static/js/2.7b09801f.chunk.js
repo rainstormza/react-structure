5bda96ebba28d861b06a59f3ea233fd478a12e57
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-26 09:48:39 +0700

    Updates

--- static/js/2.7b09801f.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{1271:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),r=n.n(a),l=n(448),u=n(447),c=n(451),o=function(e){var t=e.home,n=e.setField;return r.a.createElement("div",null,r.a.createElement("h2",null,"Home"),t.text,r.a.createElement(l.c,{placeholder:"Basic usage",onChange:function(e){return n(e.target.value)}}))};t.default=Object(u.b)(function(e){return e},{setField:c.b})(o)}});
-//# sourceMappingURL=2.7b09801f.chunk.js.map
---
+++ static/js/2.b0329ee5.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{1556:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),r=n.n(a),l=n(553),u=n(269),c=n(554),o=function(e){var t=e.home,n=e.setField;return r.a.createElement("div",null,r.a.createElement("h2",null,"Home"),t.text,r.a.createElement(l.d,{placeholder:"Basic usage",onChange:function(e){return n(e.target.value)}}))};t.default=Object(u.b)(function(e){return{home:e.home}},{setField:c.b})(o)}});
+//# sourceMappingURL=2.b0329ee5.chunk.js.map
